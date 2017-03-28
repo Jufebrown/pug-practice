eeae0bda2b962c018d638089a6ec67cf6100e7cd
Jufebrown <jufebrown@gmail.com>
2017-03-28 10:06:17 -0500

    added a route and sent data to pug template

--- app.js
+++ app.js
@@ -6,3 +6,9 @@ const app = express()
 app.use(express.static('public'))
 
 app.set(`view engine`, `pug`)
+
+const names = ["Larry", "Moe", "Curly", "Mr. Rogers"]
+
+app.get('/', (req, res, next) => {
+  res.render('index', {subtitle: `This came from my JS data`, names, loggedIn: false})
+})
